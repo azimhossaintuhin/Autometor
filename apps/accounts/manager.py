0d8d1dd9f324f8e38daefa266d21660d9c9141f7
Tuhin <www.mdtuhin456@gmail.com>
2025-05-06 08:46:18 +0600

    Implement user management and authentication features with custom user model and migrations

--- apps/accounts/manager.py
+++ apps/accounts/manager.py
@@ -1,25 +1,26 @@
 from django.contrib.auth.models import BaseUserManager
 
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        
         if not email:
-            raise ValueError('The Email field must be set')
-        if not extra_fields.get('phone'):
-            raise ValueError('The Phone field must be set')
-        
+            raise ValueError("The Email field must be set")
+        if not extra_fields.get("phone"):
+            raise ValueError("The Phone field must be set")
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff" , True)
-        extra_fields.setdefault("is_superuser",True)
-        
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
         if extra_fields.get("is_staff") is False:
             raise ValueError("Superuser must have is_staff=True.")
-        if  extra_fields.get('is_superuser') is False:
+        if extra_fields.get("is_superuser") is False:
             raise ValueError("Superuser must have is_superuser=True.")
         if not password:
             raise ValueError("Superuser must have a password.")
@@ -30,4 +31,3 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,15 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .manager import CustomUserManager
 # Create your models here.
+
 
 class Users(AbstractUser):
     username = None
     phone = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] 
+
+    USERNAME_FIELD = "email"
+    objects = CustomUserManager()
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.username
+
+
+class Userprofile(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.email
+
+
+class Token(models.Model):
+    token_type = [
+        ("v", "verification"),
+        ("p", "password_reset"),
+    ]
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+class UserWallet(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+    wallet_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.user.email
